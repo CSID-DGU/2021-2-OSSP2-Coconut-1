75ad5a2ac6d2b725103b1d5f4df67828509301f0
Park Sanghyun <p981116@naver.com>
2021-11-05 03:30:21 +0900

    11/05 powerful104 mongodb 데이터작업

--- mongodb/test.py
+++ mongodb/test.py
@@ -1,8 +1,5 @@
-import pymongo as pm
-import pandas as pd
-import certifi
 
-#클라이언트 연결
-client = pm.MongoClient('mongodb+srv://OSSPCOCONUT:coconut123@ossp-cluster.3vu4p.mongodb.net/testdb?retryWrites=true&w=majority', tlsCAFile=certifi.where())
-for name in client.list_database_names():
-    print(name)
+import regions
+
+region_list = regions.get_regions()
+print(region_list)
