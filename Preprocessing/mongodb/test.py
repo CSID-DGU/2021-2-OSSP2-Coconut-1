62fad372844857be2e1dc0f32067373336b76a27
Park Sanghyun <p981116@naver.com>
2021-11-22 00:14:37 +0900

    11/22 powerful104 commits

--- Preprocessing/mongodb/test.py
+++ Preprocessing/mongodb/test.py
@@ -6,7 +6,7 @@ import regions
 region_list = regions.get_regions()
 
 #클라이언트 연결
-client = pm.MongoClient('mongodb+srv://OSSPCOCONUT:coconut123@ossp-cluster.3vu4p.mongodb.net/test?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+client = pm.MongoClient('mongodb+srv://OSSPCOCONUT:coconut123@ossp-cluster.3vu4p.mongodb.net/feature_data?retryWrites=true&w=majority', tlsCAFile=certifi.where())
 
 db = client["feature_data"]
 col = db['서울_종로구']
--- final_debug/recommend/get_feature.py
+++ final_debug/recommend/get_feature.py
@@ -4,4 +4,4 @@ def get_f(region):
     url = requests.get("https://powerful104.pythonanywhere.com/recomm/get_f?region="+region)
     text = url.text
     data = json.loads(text)
-    return data['feature']
+    return data['features']
