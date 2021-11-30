da67a8e283b53c4ba95d24ea49add43429ae871d
Park Sanghyun <p981116@naver.com>
2021-11-30 10:46:19 +0900

    11/30 powerful104 commits

--- final_debug/recommend/get_feature.py
+++ final_debug/recommend/get_feature.py
@@ -1,8 +1,7 @@
-import json, requests 
-import certifi
+import json, requests
 
 def get_f(region):
-    url = requests.get("https://powerful104.pythonanywhere.com/recomm/get_f?region="+region, verify = certifi.where())
+    url = requests.get("https://powerful104.pythonanywhere.com/recomm/get_f?region="+region, verify = False)
     text = url.text
     data = json.loads(text)
     return data['features']
